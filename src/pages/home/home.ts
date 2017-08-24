de6e66b253138c31905024edf2a10e26b01d248a
Xiaochen Wang <ryan.wang23@hotmail.com>
2017-08-23 22:49:37 -0400

    fist commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { NewPlacePage } from '../new-place/new-place'
 
 @Component({
   selector: 'page-home',
@@ -10,5 +11,7 @@ export class HomePage {
   constructor(public navCtrl: NavController) {
 
   }
-
+  onLoadNewPlace() {
+    this.navCtrl.push(NewPlacePage);
+  }
 }
